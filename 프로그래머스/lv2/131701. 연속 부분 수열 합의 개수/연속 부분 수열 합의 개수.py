a39583aa595f21e9c4c8fca13651a0c221965af1
KimJuyoung23 <38585726+KimJuyoung23@users.noreply.github.com>
2023-09-29 23:54:28 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 354.73 ms, Memory: 92 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -0,0 +1,11 @@
+def solution(elements):
+    sums, n = [], len(elements)
+    elements += elements[:-1]
+    for i, a in enumerate(elements):
+        SUM = a
+        sums.append(SUM)
+        for b in elements[i + 1:i + n]:
+            SUM += b
+            sums.append(SUM)
+
+    return len(list(set(sums)))
